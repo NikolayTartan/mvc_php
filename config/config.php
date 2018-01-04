e71fbc06e465c94c2c55fc51212a50146e942d22
NikolayTartan <32337348+NikolayTartan@users.noreply.github.com>
2018-01-04 21:21:17 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+Config::set('site_name', 'Your Site Name');
+
+Config::set('languages', array('en', 'fr'));
+
+//Routes. Route name = method prefix
+
+Config::set('routes', array(
+    'default' => '',
+    'admin' => 'admin'
+));
+
+Config::set('default_route', 'default');
+Config::set('default_language', 'en');
+Config::set('default_controller', 'pages');
+Config::set('default_action', 'index');
+
+Config::set('db.host', 'localhost');
+Config::set('db.user', 'nick');
+Config::set('db.password', 'asdfgh-1988');
+Config::set('db.db_name', 'mvc');
+
+Config::set('salt', 'jd5jddjfdf9fdjd');
